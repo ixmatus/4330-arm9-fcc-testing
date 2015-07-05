90cae1cec1319b0be7a8c584282fa54592ce0438
Parnell Springmeyer <parnell@digitalmentat.com>
2015-07-05 15:02:45 -0500

    Implementing receiver test and a few little tweaks.

--- README.md
+++ README.md
@@ -1,13 +1,52 @@
 # Welcome!
-This repository contains a set of convenience shell scripts to be used in conjunction with a Mitsumi module,
-Broadcom 4330 WiFi+BLE chip, a Freescale i.MX280 ARM9, a Linux 3.16 Kernel, with the brcmfmac kernel driver,
-and a custom user-space `wl` tool provided by Mitsumi.
+This repository contains a convenience shell script and go program to be used in conjunction with a Mitsumi
+module, Broadcom 4330 WiFi+BLE chip, a Freescale i.MX280 ARM9, a Linux 3.16 Kernel, with the brcmfmac kernel
+driver, and a custom user-space `wl` tool provided by Mitsumi.
 
-The scripts will activate and turn on the test mode commands. You will need the proprietary docs from your
-vendor but here is "folklore circulated" set of commands for activating packet transmission or reception for
-regulatory testing.
+## WiFi Test Mode
+The shell script `wifi-testmode.sh` will configure and activate the 4330 chip for the recommended United
+States regulatory test requirements. You will need the proprietary docs from your vendor but this script is
+a "folklore circulated" set of commands for activating packet transmission or reception for regulatory
+testing.
 
-Only use these as a reference (I slogged through this), you should definitely be double checking anything you have to do for FCC testing with your vendor.
+Only use these as a reference (I slogged through this), you should definitely be double checking anything
+you have to do for FCC testing with your vendor.
+
+I wanted to simplify the usage significantly and here is sample output of the help message:
+
+```
+» ./wifi-testmode.sh --help
+BCM4330 intentional Wi-Fi transmission testing
+
+NOTE: the manufacturer binary image must be downloaded onto the 4330
+in order for any of these commands to work! The 4330 also does not
+support MIMO mode so no MIMO commands are issued in this script.
+
+Usage of ./wifi-testmode.sh
+        -h --help
+        --channel=1     (1..14)
+        --duration=10   (0 is continuous, >=1 is duration in seconds)
+        --mode=b        (b,g,n)
+        --power=69      (quarter dB values)
+        --state=tx      (tx,rx)
+```
+
+## BLE Test Mode
+The Golang program also in this repository will configure and activate the 4330 chip to test reception and transmission for United States regulatory test requirements.
+
+Only use these as a reference (I slogged through this), you should definitely be double checking anything
+you have to do for FCC testing with your vendor.
+
+```
+» ./ble-testmode --help
+Usage of ./ble-testmode:
+  -channel=0: Transmission channel (1..79)
+  -duration=10s: Transmission duration (0 is continuous, >=1 is duration in seconds)
+  -length=0: Length of the test data
+  -payload=0: Packet payload
+  -state="tx": Are we transmitting or receiving? (tx | rx)
+  -stop=false: If a test was started without a duration of zero, issue this command to halt it
+```
 
 ## Broadcom 4330 Firmware note
 The Broadcom 4330 is a dynamically programmed chip, you have to download firmware to it. None of the test commands in this repository will work if you are providing the production version of the 4330's binary image to the kernel driver. *You must have the manufacturer binary firmware image* to load onto the chip in order for any of the `wl` utility test-mode commands to work.
--- ble-testmode.go
+++ ble-testmode.go
@@ -5,36 +5,62 @@ import (
     "log"
     "time"
     "bytes"
+    "os"
     "github.com/plumlife/gatt"
     "github.com/plumlife/gatt/linux/cmd"
 )
 
 var length, payload, channel uint
 var duration time.Duration
+var state string
+var stop bool
 
 func main(){
     // Parse CLI arguments
     flag.UintVar(&channel,      "channel",  0, "Transmission channel (1..79)")
     flag.DurationVar(&duration, "duration", (time.Second * time.Duration(10)), "Transmission duration (0 is continuous, >=1 is duration in seconds)")
+    flag.StringVar(&state,      "state",    "tx", "Are we transmitting or receiving? (tx | rx)")
     flag.UintVar(&length,       "length",   0, "Length of the test data")
     flag.UintVar(&payload,      "payload",  0x00, "Packet payload")
+    flag.BoolVar(&stop,         "stop",     false, "If a test was started without a duration of zero, issue this command to halt it")
     flag.Parse()
     
     log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-    
-    // If the help switch has been given, print out the help message
-    log.Println("Initializing BLE intentional transmission... ")
 
-    device := StartBLETransmitterTest(duration, uint8(channel), uint8(length), uint8(payload))
-
-    if duration > 0 {
-        time.Sleep(time.Second * time.Duration(duration))
+    if stop {
+        device, _ := gatt.NewDevice()
+        StopBLETest(device, uint8(payload))
+        os.Exit(0)
     }
-        
-    StopBLETransmitterTest(device)
+    
+    if state == "tx" {
+        log.Println("Initializing BLE intentional transmission... ")
+        device := StartBLETransmitterTest(duration, uint8(channel), uint8(length), uint8(payload))
+        WaitAndStop(duration, device, uint8(payload))
+    } else if state == "rx" {
+        log.Println("Initializing BLE intentional reception...")
+        device := StartBLEReceiverTest(duration, uint8(channel), uint8(payload))
+        WaitAndStop(duration, device, uint8(payload))
+    } else {
+        log.Println("Unrecognized state")
+        os.Exit(1)
+    }
 }
 
-func StartBLETransmitterTest(duration time.Duration , channel uint8, length uint8, payload uint8) (gatt.Device) {
+// Given a duration in seconds, sleep, then halt the test. If duration
+// is zero, the test will never be halted and must be done so manually.
+func WaitAndStop(duration time.Duration, device gatt.Device, payload uint8) {
+    if duration > 0 {
+        time.Sleep(time.Second * time.Duration(duration))
+        StopBLETest(device, payload)
+    } else {
+        log.Println("WARNING no duration given, you must manually halt the test with: ble-testmode --stop --payload=0x00 (or whatever the original payload was)")
+    }
+}
+
+// Given a duration, channel, payload length, and packet payload
+// initialize a transmission test.
+func StartBLETransmitterTest(duration time.Duration, channel uint8, length uint8, payload uint8) (gatt.Device) {
     log.Println("duration is ", duration, "channel is ", channel)
     
     device, _ := gatt.NewDevice()
@@ -51,20 +77,47 @@ func StartBLETransmitterTest(duration time.Duration , channel uint8, length uint
     
     device.Option(gatt.LnxSendHCIRawCommand(c, rsp))
     
-    if rsp.Bytes()[0] != 0x00 {
-        log.Println("Response bytes not equal to 0x00, this is unexpected")
+    if rsp.Bytes()[0] != payload {
+        log.Println("Response bytes (%d) not equal to %d, this is unexpected", rsp.Bytes()[0], payload)
     }
     
     return device
 }
 
-func StopBLETransmitterTest(device gatt.Device){
+// Given a duration, a channel, and a payload (to compare against what
+// was received) initialize a receiver test.
+func StartBLEReceiverTest(duration time.Duration, channel uint8, payload uint8) (gatt.Device) {
+    log.Println("duration is ", duration, "channel is ", channel)
+
+    device, _ := gatt.NewDevice()
+    
+    // Reset the chip
+    reset := &cmd.Reset{}
+
+    rsp := bytes.NewBuffer(nil)
+    
+    device.Option(gatt.LnxSendHCIRawCommand(reset,rsp))
+    
+    // Begin receiving
+    c := &cmd.LEReceiverTest{RxChannel:channel}
+
+    device.Option(gatt.LnxSendHCIRawCommand(c, rsp))
+    
+    if rsp.Bytes()[0] != payload {
+        log.Println("Response bytes (%d) not equal to %d, this is unexpected", rsp.Bytes()[0], payload)
+    }
+    
+    return device
+}
+
+// Halt either a receiver or transmission test.
+func StopBLETest(device gatt.Device, payload uint8){
     rsp     := bytes.NewBuffer(nil) 
     cmd_end := &cmd.LETestEnd{}
     
     device.Option(gatt.LnxSendHCIRawCommand(cmd_end,rsp))
     
-    if rsp.Bytes()[0] != 0x00 {
+    if rsp.Bytes()[0] != payload {
         log.Println("Response bytes not equal to 0x00, this is unexpected")
     }   
 }
